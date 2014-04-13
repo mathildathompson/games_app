c109b2c12e65871caee16b025480da8ed0733fc7
Erik Froese <froesecom@gmail.com>
2014-04-13 19:56:03 +1000

    increased game size. made the camera fixed to the player. added some ledges

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
         function preload() {
 
-            game.load.image('sky', '/assets/sky.png');
+            game.load.image('sky', '/assets/sky_large.png');
             game.load.image('ground', '/assets/platform.png');
             game.load.image('star', '/assets/star.png');
             game.load.spritesheet('dude', '/assets/dude.png', 32, 48);
@@ -20,7 +20,7 @@ $(document).ready(function() {
         var scoreText;
 
         function create() {
-
+            game.world.setBounds(0, 0, 1200, 900);
             //  We're going to be using physics, so enable the Arcade Physics system
             game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -37,7 +37,7 @@ $(document).ready(function() {
             var ground = platforms.create(0, game.world.height - 64, 'ground');
 
             //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-            ground.scale.setTo(2, 2);
+            ground.scale.setTo(4, 2);
 
             //  This stops it from falling away when you jump on it
             ground.body.immovable = true;
@@ -51,7 +51,6 @@ $(document).ready(function() {
             ledge = platforms.create(450, 300, 'ground');
             ledge.body.immovable = true;
 
-            
 
             //the two below are on the left
             //the first number says how many pixels from the left border it is
@@ -65,7 +64,10 @@ $(document).ready(function() {
             ledge = platforms.create(-200, 250, 'ground');
             ledge.body.immovable = true;
 
-            ledge = platforms.create(-300, 100, 'ground');
+            ledge = platforms.create(0, 700, 'ground');
+            ledge.body.immovable = true;
+
+            ledge = platforms.create(350, 600, 'ground');
             ledge.body.immovable = true;
 
             // The player and its settings
@@ -82,7 +84,7 @@ $(document).ready(function() {
             //  Our two animations, walking left and right.
             player.animations.add('left', [0, 1, 2, 3], 10, true);
             player.animations.add('right', [5, 6, 7, 8], 10, true);
-
+            game.camera.follow(player);
             //  Finally some stars to collect
             stars = game.add.group();
 
