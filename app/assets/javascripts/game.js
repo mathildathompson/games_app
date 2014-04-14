cc9a6eaea7651ba49017b3d1938837cacf3bc683
thomasdane <tjdane@gmail.com>
2014-04-14 14:33:42 +1000

    powerup when player collect object

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
             game.load.image('sky', '/assets/forest.png');
             game.load.image('ground', '/assets/platform.png');            
             game.load.image('star', '/assets/star.png');
+            game.load.image('power', '/assets/powerup.png');
             //here we load two more assets for the other 'stars'
             game.load.image('diamond', '/assets/diamond.png');
             game.load.image('shinyball', '/assets/shinyball.png');
@@ -18,7 +19,7 @@ $(document).ready(function() {
         var player;
         var platforms;
         var cursors;
-
+        var powerup
         var stars;
         //here we set two more vars
         var score = 0;
@@ -229,6 +230,16 @@ $(document).ready(function() {
                     $section.prev('h3').css('background', 'linear-gradient(#272822, #3B3A32)').fadeIn(400);
                 });
             }
+
+            powerup = game.add.sprite(player.body.x - 32, player.body.y - 32,  'powerup');
+            explosion.animations.add('powerup', [0, 1, 2, 3, 4, 5], 10, true);
+            explosion.animations.play('powerup');
+            // setTimeout(powerup, 1500);
+
+            // function explode() {
+            //   explosion.kill();  
+            // }
+            
 
             highlight();
 
