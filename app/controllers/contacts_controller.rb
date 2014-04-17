0e4e4ee478a6f8a61e2864cf094ef0c95f69cdc5
Erik Froese <froesecom@gmail.com>
2014-04-17 14:55:19 +1000

    updated homepage links to contacts. updated contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,8 +3,10 @@ class ContactsController < ApplicationController
   def index
     @user = User.find_by last_name: params[:last_name].capitalize
     @first_name = @user.first_name
+    @last_name = @user.last_name
     @github = @user.github
     @linkedin = @user.linkedin
+    @twitter = @user.twitter
 
   end
 
