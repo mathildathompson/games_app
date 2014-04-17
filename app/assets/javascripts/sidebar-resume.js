806e6cb9da464c319f98b63e923aa3bae77c19d1
Erik Froese <froesecom@gmail.com>
2014-04-17 14:59:01 +1000

    removed unnecessary files

--- app/assets/javascripts/sidebar-resume.js
+++
@@ -1,7 +0,0 @@
-//this file controls the sidebar that displays the resume
-//it uses jquery to add content into the sidebar-resume div in the DOM
-//these events are triggered by code in the phaser.js/index.html
-//in this file is the jQuery that controls the accordion effect. It interacts with the content in the 
-//sidebar-resume div in the games index view. 
-
-/*jQuery time*/
